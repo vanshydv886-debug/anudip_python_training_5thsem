9e593d4f85212fd0ad9da6e58d27b2752c0af71c
vansh yadav <vanshydv886@gmail.com>
2026-06-23 16:53:22 +0530

    iteration_4thjune

--- classrom/iteration_4th_june/battery_charge.py
+++ classrom/iteration_4th_june/battery_charge.py
@@ -1,6 +1,6 @@
 #program  for displaying battery charging level 
-charge_level = 20
-while(chargeing_level<= 100)  #isme time nhi pt h but battery level ya percent pt h
+charging_level = 20
+while(charging_level<= 100) :    #isme time nhi pt h but battery level ya percent pt h
     if(charging_level):
         print("battery level : " , charging_level, "%")
         charging_level = charging_level + 10
--- classrom/iteration_4th_june/watertank.py
+++ classrom/iteration_4th_june/watertank.py
@@ -6,7 +6,10 @@ water = 0
 
 # Continue until tank reaches 100 liters
 while water < 100:
-    water = water + 10  # increase water level by 10 liters each minute
+    water = water + 10  # increase water level by 10 liters each minute  # water += 10
     print("Water Level:", water, "liters")
 
 print("Tank is full.") 
+
+#for lopp when u know no of repition it just iterate sequence
+#when no knowledge of squence but it iterate till statement is true
